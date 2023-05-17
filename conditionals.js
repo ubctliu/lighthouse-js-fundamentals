4112bc7962da5d36b1a72d6c781be64f67785a7e
Terrence <terrence.liu@alumni.ubc.ca>
2023-05-16 20:25:57 -0700

    Use conditionals to tell people about the weather

---
+++ conditionals.js
@@ -0,0 +1,17 @@
+const raining = true;
+const temperature = 12;
+
+if (raining) {
+	console.log("Don't forget your umbrella!");
+}
+
+if (temperature < 0) {
+	console.log("Make sure you pick out a scarf!");
+} else 
+if (temperature < 15) {
+	console.log("short sleeves won't cut it!");
+} else {
+	console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
